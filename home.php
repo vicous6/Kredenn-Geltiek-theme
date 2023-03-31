fc94b92dc53f911183d3e6217a53f3961bc5a6fc
David <davidsim1035@gmail.com>
2023-03-31 10:19:34 +0200

    Adding all pages

--- home.php
+++ home.php
@@ -1,39 +1,9 @@
 <?php
 
 get_header(); 
-<<<<<<< HEAD
 
 ?>
 
-<h1><?php the_field('titre'); ?></h1>
-<?php the_field('contenu'); ?>
-
-    <div class="site__blog">
-        <main class="site__content">
-            <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-                <article class="post">
-                    <h2><?php the_title(); ?></h2>
-                    <?php the_post_thumbnail(); ?>
-                    <?php the_excerpt(); ?>
-                    <p>
-                        <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
-                    </p>
-                </article>
-            <?php endwhile; endif; ?>
-            <div class="site__navigation">
-                <div class="site__navigation__prev">
-                    <?php previous_posts_link( 'Page Précédente' ); ?>
-                </div>
-                <?php the_posts_pagination(); ?>
-                <div class="site__navigation__next">
-                    <?php next_posts_link( 'Page Suivante' ); ?> 
-                </div>
-            </div>
-        </main>
-=======
->>>>>>> e78f75f49821b3f36a6730f76d5c73d9152ada2a
-
-?>
 <section>
     <h1><?php the_field('titre'); ?></h1>
     <p><?php the_field('contenu'); ?></p>
@@ -67,6 +37,7 @@ get_header();
         </ul>
     </aside>
 </div> 
+
 <?php 
 
 get_footer(); 
