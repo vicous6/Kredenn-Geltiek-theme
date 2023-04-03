88eb1d4d5b910ac644626a3e5ee7f0fd9e8957bd
David <davidsim1035@gmail.com>
2023-04-03 17:37:52 +0200

    Adding navbar and accueil css

--- header.php
+++ header.php
@@ -13,13 +13,22 @@ $menuItems = getNavigationMenu();
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" />
         <title><?php echo get_bloginfo( 'name' );?></title>
     </head>
     <body>
-        <header>
-        <?php wp_nav_menu( array( 'theme_location' => 'Navigation' ) ); ?>
+        <header class="header">
+            <aside>
+                <a href="#" class="logo">
+                    <img src="https://static.wixstatic.com/media/84770f_042ffa30c947807e7a43c51fa981b720.png/v1/fill/w_155,h_154,al_c,lg_1,q_85,enc_auto/84770f_042ffa30c947807e7a43c51fa981b720.png">
+                </a>
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Enseignement</a></li>
+                    <li><a href="#">Les Actions</a></li>
+                </ul>
+            </aside>
         </header>
         
     
