11e81592801fc6729870d23aa239e99296bd1d90
fvzevrvze cezceacez <vicous@free.fr>
2023-04-03 16:41:09 +0200

    mise a jjour sass

---
+++ page-kredenn-geltiek.php
@@ -0,0 +1,16 @@
+<?php  
+/*
+Template Name: Kredenn Geltiek
+*/  
+get_header();
+
+?>
+<main id = "kredenn-geltiek">
+<h1><?php the_field('titre'); ?></h1>
+<?php the_field('contenu'); ?>
+</main>
+<?php  
+
+get_footer(); 
+
+?>
