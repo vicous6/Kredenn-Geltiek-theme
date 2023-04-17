e649bd827ac469dbe99522ed37983013b5bd0f32
fvzevrvze cezceacez <vicous@free.fr>
2023-04-17 11:54:32 +0200

    fix

--- page-une-clairiere.php
+++ page-une-clairiere.php
@@ -21,13 +21,13 @@ get_header();
    </figure>
 </section>
 <section>
-<p><?php the_field('contact'); ?>" </p>
+<p><?php the_field('contact'); ?> </p>
 <p><?php the_field('lieu'); ?>" </p>
 <p><?php the_field('description'); ?>" </p>
 </section>
 <section>
     <h2>Informations Spécifiques</h2>
-<p><?php the_field('information-specifique'); ?>" </p>
+<p><?php the_field('information-specifique'); ?> </p>
 <p><?php the_field('lieu'); ?>" </p>
 </section>
 <section>
