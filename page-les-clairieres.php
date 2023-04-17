38446baada4985a0bfc88b791b131a96d8e366f2
Toznyy <anthony.cormier29860@gmail.com>
2023-04-17 16:24:16 +0200

    ajout de la carte des clairières

--- page-les-clairieres.php
+++ page-les-clairieres.php
@@ -6,9 +6,16 @@ get_header();
 
 ?>
 <main id = "clairieres">
-<h1><?php the_field('titre'); ?></h1>
-<?php the_field('contenu'); ?>
-<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1YCU1vq-e7ptWp2Kd0biCiy9xzTZRGlc&ehbc=2E312F" width="640" height="480"></iframe></main>
+    <h1><?php the_field('titre'); ?></h1>
+    <section>
+        <section>
+            <?php the_field('contenu'); ?>
+        </section>
+        <section>
+            <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1YCU1vq-e7ptWp2Kd0biCiy9xzTZRGlc&ehbc=2E312F" width="640" height="480"></iframe>
+        </section>
+    </section>
+</main>
 <?php  
 
 get_footer(); 
