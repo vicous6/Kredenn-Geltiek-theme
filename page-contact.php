a08e2c525ef9753eb60eae291e2134fc9e214e8c
David <davidsim1035@gmail.com>
2023-04-18 14:12:28 +0200

    Mise à jour de la page d'accueil

--- page-contact.php
+++ page-contact.php
@@ -10,8 +10,8 @@ get_header();
     <h1><?php the_field('titre'); ?></h1>
     <p><?php the_field('contenu'); ?></p>
     <ul>
-        <li><a href="https://www.youtube.com/channel/UCdVeNteVa2wF4LAVDn6VLsA?app=desktop"><span class="fa-brands fa-youtube fa-2xl" style="color: #e11414;"></span></a></li>
-        <li><a href="https://m.facebook.com/kredenngeltiek/"><span class="fa-brands fa-facebook fa-2xl" style="color: #0d52c9;"></span></a></li>
+        <li><a href="https://www.youtube.com/channel/UCdVeNteVa2wF4LAVDn6VLsA?app=desktop"><span class="fa-brands fa-youtube fa-4x" style="color: #e11414;"></span></a></li>
+        <li><a href="https://m.facebook.com/kredenngeltiek/"><span class="fa-brands fa-facebook fa-4x" style="color: #0d52c9;"></span></a></li>
     </ul>
 </section>  
 </main>
