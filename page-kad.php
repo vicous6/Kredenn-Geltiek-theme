c478de8fcd185af2a48adf975e2b21eab5f148ff
Toznyy <anthony.cormier29860@gmail.com>
2023-04-21 16:25:44 +0200

    modification de la page kad

--- page-kad.php
+++ page-kad.php
@@ -7,6 +7,9 @@ get_header();
 ?>
 <main id = "kad">
 <h1><?php the_field('titre'); ?></h1>
+<figure>
+    <img src = <?php the_field('image1'); ?> >
+</figure>
 <?php the_field('contenu'); ?>
 </main>
 <?php  
