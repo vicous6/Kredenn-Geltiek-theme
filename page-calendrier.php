1e650a6a54996c43215b668f9f1759d63d6405a8
Toznyy <anthony.cormier29860@gmail.com>
2023-04-21 14:29:44 +0200

    modification sass calendrier

--- page-calendrier.php
+++ page-calendrier.php
@@ -8,7 +8,7 @@ get_header();
 <main id="calendrier">
 <section>
     <h1>Calendrier</h1>
-    <p>Les Clairières fêtent seules ou ensemble les huit cérémonies calendaires. Ces huit fêtes sont : Samonios, Genimalacta, Ambivolcios, Satios, Belotennia, Mediosamonios, Lugunaissatis, Tiocobrixtio. Les lieux sont au choix des Clairières, certaines préférant toujours l'extérieur, d'autres les lieux couverts en cas d'intempéries.
+    <p>Les Clairières fêtent seules ou ensemble les huit cérémonies calendaires. Ces huit fêtes sont : Samonios, Genimalacta, Ambivolcios, Satios, Belotennia, Mediosamonios, Lugunaissatis, Tiocobrixtio. Les lieux sont au choix des Clairières, certaines préférant toujours l'extérieur, d'autres les lieux couverts en cas d'intempéries. 
     Les huit fêtes calendaires ont lieu selon un rythme qui s'inspire du calendrier de Coligny.
 
     Vous trouverez également dans ce calendrier les annonces pour tous les autres événements tels que conférence publique, condate, Skol an Drouiz, Poelgor Nevet, Gorsedd et Mediolanon.
